99c9ac87a775a14b46d2b49bc38f902de4510c4f
karlavizcarram <kvizcarra27@gmail.com>
2025-09-14 16:22:36 -0600

    Se genera array para almacenar nombres

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Crea array de almacenamiento de amigos
+let listaAmigos = []
