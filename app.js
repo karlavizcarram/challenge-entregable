a9fd24eb8a6a12701bc8b94e20517ef4fbdbf141
karlavizcarram <kvizcarra27@gmail.com>
2025-09-14 16:23:18 -0600

    Se genera funcion para agregar amigos

--- app.js
+++ app.js
@@ -2,3 +2,18 @@
 
 // Crea array de almacenamiento de amigos
 let listaAmigos = []
+
+// Funcion para agregar amigos 
+function agregarAmigo() {
+    // Agrega el valor de el id "amigo" a la variable inputValor
+    let inputValor = document.getElementById("amigo")
+    // Valida que no esté vacio y agrega a la lista
+    if (inputValor.value !="") {
+        listaAmigos.push(inputValor.value)
+        // Limpia variable input después de agregar a lista
+        inputValor.value = ""
+        console.log(listaAmigos)
+    } else {
+        alert("Por favor ingresa un nombre")
+    }
+}
